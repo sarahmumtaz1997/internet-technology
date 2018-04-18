22694ccdd64d9cecf4a1de16bf3055f2be120e04
sarahmumtaz1997 <38484836+sarahmumtaz1997@users.noreply.github.com>
2018-04-18 10:00:24 +0530

    Create vote.php

---
+++ vote.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+	<form id="myform">
+	<input type="radio" name="candidate" id="name1" value="Sarah"/>Sarah<br />
+	<form id="myform">
+	<input type="radio" name="candidate" id="name2" value="fathima" />fathima<br />
+	<form id="myform">
+	<input type="radio" name="candidate" id="name3" value="arfa"/>arfa<br />
+	<form id="myform">
+	<input type="radio" name="candidate" id="name4" value="sadiya"/>sadiya<br />
+
+	</form>
+
+
+</body>
+</html>
