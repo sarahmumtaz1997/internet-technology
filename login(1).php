17f74a0c9667381fa05ba1cd85980d46a01e0ae9
sarahmumtaz1997 <38484836+sarahmumtaz1997@users.noreply.github.com>
2018-04-18 10:02:13 +0530

    Create login(1).php

---
+++ login(1).php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+$username=$_POST['username'];
+$password=$_POST['password'];
+$db=mysqli_connect('localhost','root','tiger','2015cse150') or die("error connecting to database".mysqli_error($db));
+
+$query="select username,password from login where username='$username' and password='$password'";
+$result=mysqli_query($db,$query) or die("error querying database");
+$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+if($row==TRUE)
+{
+header('location:vote.html');
+}
+else
+{
+echo "incorrect username or password";
+}
